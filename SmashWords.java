c527f5a52b0083ab832257fa69346f1357173d62
KrasnovaElena <89896503+KrasnovaElena@users.noreply.github.com>
2022-03-28 01:10:51 +0300

    Add files via upload

---
+++ SmashWords.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+/*Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word.
+ Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+ */
+public class SmashWords {
+
+    public static String smash(String... words) {
+        // TODO Write your code below this comment please
+        return String.join(" ", words);
+    }
+}
